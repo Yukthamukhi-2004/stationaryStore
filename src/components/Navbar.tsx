a9afaea096f79ea89adbf115a32626da17ea37ae
Yuktha <yukthakodumuru@gmail.com>
2026-06-04 20:37:11 +0530

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-brand">
-          Store
+          Stationary
         </Link>
 
         <div className="navbar-links">
